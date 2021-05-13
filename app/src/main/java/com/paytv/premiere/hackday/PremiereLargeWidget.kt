b3028436e47df6a5bb6bf09fffcbbbf6f92e741a
Larissa Marães <larissa.maraes@gmail.com>
2021-05-13 16:22:14 -0300

    Adjusting small and large widgets by layouts

--- app/src/main/java/com/paytv/premiere/hackday/PremiereLargeWidget.kt
+++ app/src/main/java/com/paytv/premiere/hackday/PremiereLargeWidget.kt
@@ -3,6 +3,7 @@ package com.paytv.premiere.hackday
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.os.Bundle
 import android.widget.RemoteViews
 
 /**
@@ -23,14 +24,65 @@ class PremiereLargeWidget : AppWidgetProvider() {
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
     }
+
+    override fun onAppWidgetOptionsChanged(context: Context?, appWidgetManager: AppWidgetManager?, appWidgetId: Int, newOptions: Bundle?) {
+        // See the dimensions and
+        val options = appWidgetManager!!.getAppWidgetOptions(appWidgetId)
+
+        // Get min width and height.
+        val minWidth = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH)
+        val minHeight = options
+            .getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT)
+
+        // Obtain appropriate widget and update it.
+        appWidgetManager.updateAppWidget(
+            appWidgetId,
+            getRemoteViews(context!!, minWidth, minHeight)
+        )
+
+        super.onAppWidgetOptionsChanged(
+            context, appWidgetManager, appWidgetId,
+            newOptions
+        )
+
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions)
+    }
 }
 
 internal fun updateAppLargeWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
     val widgetText = context.getString(R.string.appwidget_text)
     // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.premiere_large_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
+    val views = RemoteViews(context.packageName, R.layout.premiere_widget_2x2)
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
+}
+
+private fun getRemoteViews(
+    context: Context,
+    minWidth: Int,
+    minHeight: Int
+): RemoteViews {
+    // First find out rows and columns based on width provided.
+    val rows = getCellsForSize(minHeight)
+    val columns = getCellsForSize(minWidth)
+    if (columns == 2) {
+        return RemoteViews(
+            context.packageName,
+            R.layout.premiere_widget_2x2
+        )
+    }
+    // Get appropriate remote view.
+    return RemoteViews(
+        context.packageName,
+        R.layout.premiere_widget_2x3
+    )
+}
+
+private fun getCellsForSize(size: Int): Int {
+    var n = 2
+    while (70 * n - 30 < size) {
+        ++n
+    }
+    return n - 1
 }
--- app/src/main/java/com/paytv/premiere/hackday/PremiereSmallWidget.kt
+++ app/src/main/java/com/paytv/premiere/hackday/PremiereSmallWidget.kt
@@ -3,7 +3,6 @@ package com.paytv.premiere.hackday
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
-import android.os.Bundle
 import android.widget.RemoteViews
 
 /**
@@ -28,29 +27,6 @@ class PremiereSmallWidget : AppWidgetProvider() {
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
     }
-
-    override fun onAppWidgetOptionsChanged(context: Context?, appWidgetManager: AppWidgetManager?, appWidgetId: Int, newOptions: Bundle?) {
-        // See the dimensions and
-        val options = appWidgetManager!!.getAppWidgetOptions(appWidgetId)
-
-        // Get min width and height.
-        val minWidth = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH)
-        val minHeight = options
-            .getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT)
-
-        // Obtain appropriate widget and update it.
-        appWidgetManager.updateAppWidget(
-            appWidgetId,
-            getRemoteViews(context!!, minWidth, minHeight)
-        )
-
-        super.onAppWidgetOptionsChanged(
-            context, appWidgetManager, appWidgetId,
-            newOptions
-        )
-
-        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions)
-    }
 }
 
 internal fun updateAppWidget(
@@ -66,66 +42,4 @@ internal fun updateAppWidget(
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
-}
-
-/**
- * Determine appropriate view based on width provided.
- *
- * @param minWidth
- * @param minHeight
- * @return
- */
-private fun getRemoteViews(
-    context: Context,
-    minWidth: Int,
-    minHeight: Int
-): RemoteViews {
-    // First find out rows and columns based on width provided.
-    val rows = getCellsForSize(minHeight)
-    val columns = getCellsForSize(minWidth)
-    if (rows == 1) {
-        return RemoteViews(
-            context.packageName,
-            R.layout.premiere_widget_1x2
-        )
-    }
-    if (rows == 2) {
-        if (columns == 2) {
-            return RemoteViews(
-                context.packageName,
-                R.layout.premiere_widget_2x2
-            )
-        }
-        if (columns == 3) {
-            return RemoteViews(
-                context.packageName,
-                R.layout.premiere_widget_2x3
-            )
-        }
-        if (columns == 4) {
-            return RemoteViews(
-                context.packageName,
-                R.layout.premiere_widget_2x3
-            )
-        }
-    }
-    // Get appropriate remote view.
-    return RemoteViews(
-        context.packageName,
-        R.layout.premiere_widget_2x3
-    )
-}
-
-/**
- * Returns number of cells needed for given size of the widget.
- *
- * @param size Widget size in dp.
- * @return Size in number of cells.
- */
-private fun getCellsForSize(size: Int): Int {
-    var n = 2
-    while (70 * n - 30 < size) {
-        ++n
-    }
-    return n - 1
 }
