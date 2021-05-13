2679f81a3aa297c376349197f2f8de0e4a5a5baa
Larissa Marães <larissa.maraes@gmail.com>
2021-05-13 19:24:39 -0300

    Loading content on small widget

--- app/src/main/java/com/paytv/premiere/hackday/PremiereSmallWidget.kt
+++ app/src/main/java/com/paytv/premiere/hackday/PremiereSmallWidget.kt
@@ -3,18 +3,30 @@ package com.paytv.premiere.hackday
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.graphics.Bitmap
+import android.os.Bundle
 import android.widget.RemoteViews
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.AppWidgetTarget
+import com.bumptech.glide.request.transition.Transition
+import com.paytv.premiere.core.entities.Match
+import com.paytv.premiere.core.service.MatchesServiceImpl
 
-/**
- * Implementation of App Widget functionality.
- */
 class PremiereSmallWidget : AppWidgetProvider() {
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
-        // There may be multiple widgets active, so update all of them
+        matchesService = MatchesServiceImpl(context)
+        matches = matchesService.getMatches()
+
+        loadContent(
+            context,
+            appWidgetIds,
+            matches[0],
+            RemoteViews(context.packageName, R.layout.premiere_widget_1x2)
+        )
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
         }
@@ -27,6 +39,21 @@ class PremiereSmallWidget : AppWidgetProvider() {
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
     }
+
+    override fun onAppWidgetOptionsChanged(
+        context: Context?,
+        appWidgetManager: AppWidgetManager?,
+        appWidgetId: Int,
+        newOptions: Bundle?
+    ) {
+        loadContent(
+            context!!,
+            IntArray(appWidgetId),
+            matches[0],
+            RemoteViews(context.packageName, R.layout.premiere_widget_1x2)
+        )
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions)
+    }
 }
 
 internal fun updateAppWidget(
@@ -34,10 +61,37 @@ internal fun updateAppWidget(
     appWidgetManager: AppWidgetManager,
     appWidgetId: Int
 ) {
-    val widgetText = context.getString(R.string.appwidget_text)
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.premiere_widget_1x2)
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
+}
+
+private fun loadContent(
+    context: Context,
+    appWidgetId: IntArray,
+    match: Match,
+    remoteViews: RemoteViews
+) {
+    val homeTeamLogo: AppWidgetTarget = object :
+        AppWidgetTarget(context.applicationContext, R.id.homeTeamLogo, remoteViews, *appWidgetId) {
+        override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+            super.onResourceReady(resource, transition)
+        }
+    }
+
+    val awayTeamLogo: AppWidgetTarget = object :
+        AppWidgetTarget(context.applicationContext, R.id.awayTeamLogo, remoteViews, *appWidgetId) {
+        override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+            super.onResourceReady(resource, transition)
+        }
+    }
+
+    Glide.with(context).asBitmap().load(match.home!!.image).into(homeTeamLogo)
+    Glide.with(context).asBitmap().load(match.away!!.image).into(awayTeamLogo)
+
+    remoteViews.setTextViewText(R.id.scoreText, "${match.homeScore} X ${match.awayScore}")
+    remoteViews.setTextViewText(R.id.awayTeamText, match.away!!.abbreviation)
+    remoteViews.setTextViewText(R.id.homeTeamText, match.home!!.abbreviation)
 }
