3b0c514179b6de6d812990c486c3d50051b63293
Gustavo de Almeida <gustavopcdealmeida@gmail.com>
2021-05-13 20:06:24 -0300

    Updating pager item

--- smartwatch/src/main/java/com/paytv/premiere/smartwatch/adapter/MatchesPagerAdapter.kt
+++ smartwatch/src/main/java/com/paytv/premiere/smartwatch/adapter/MatchesPagerAdapter.kt
@@ -1,8 +1,10 @@
 package com.paytv.premiere.smartwatch.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
 import com.paytv.premiere.core.entities.Match
 import com.paytv.premiere.smartwatch.databinding.ItemMatchDetailBinding
 
@@ -20,7 +22,7 @@ class MatchesPagerAdapter(
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as MatchPageViewHolder).bind()
+        (holder as MatchPageViewHolder).bind(list[position])
     }
 
     override fun getItemCount() = list.size
@@ -29,8 +31,28 @@ class MatchesPagerAdapter(
         private val binding: ItemMatchDetailBinding
     ) : RecyclerView.ViewHolder(binding.root) {
 
-        fun bind() {
+        fun bind(match: Match) {
             with(binding) {
+
+                if (match.status == "live") {
+                    binding.liveImageView.visibility = View.VISIBLE
+                    binding.itemMatchDetailTime.visibility = View.VISIBLE
+                    binding.itemMatchDetailMatchHour.visibility = View.GONE
+                    binding.itemMatchDetailMatchInformation.visibility = View.GONE
+                } else {
+                    binding.liveImageView.visibility = View.GONE
+                    binding.itemMatchDetailTime.visibility = View.GONE
+                    binding.itemMatchDetailMatchHour.visibility = View.VISIBLE
+                    binding.itemMatchDetailMatchInformation.visibility = View.VISIBLE
+                }
+
+                homeTeamScore.text = match.homeScore.toString()
+                itemMatchDetailHomeTeamName.text = match.home?.abbreviation
+                itemMatchDetailHomeTeamIcon.load(match.home?.image)
+
+                awayTeamScore.text = match.awayScore.toString()
+                itemMatchDetailAwayTeamName.text = match.away?.abbreviation
+                itemMatchDetailAwayTeam.load(match.away?.image)
             }
         }
     }
