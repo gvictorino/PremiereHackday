3c8d3908975060b90a8523eac0ccae78fecf6882
Gustavo de Almeida <gustavopcdealmeida@gmail.com>
2021-05-13 15:58:14 -0300

    PagerMatches activity created

---
+++ smartwatch/src/main/java/com/paytv/premiere/smartwatch/PagerMatchesActivity.kt
@@ -0,0 +1,53 @@
+package com.paytv.premiere.smartwatch
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.paytv.premiere.smartwatch.databinding.ActivityPagerMatchesBinding
+import com.paytv.premiere.smartwatch.databinding.PageMatchBinding
+
+class PagerMatchesActivity : Activity() {
+
+    private lateinit var binding: ActivityPagerMatchesBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPagerMatchesBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
+        binding.matchesViewPager.adapter = MatchesPagerAdapter(listOf(1, 2, 3))
+    }
+
+    class MatchesPagerAdapter(
+        private val list: List<Any>
+    ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            return MatchPageViewHolder(
+                PageMatchBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            (holder as MatchPageViewHolder).bind()
+        }
+
+        override fun getItemCount() = list.size
+    }
+
+    class MatchPageViewHolder(
+        private val binding: PageMatchBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind() {
+            with(binding) {
+                text.text = "Page $adapterPosition"
+            }
+        }
+    }
+}
