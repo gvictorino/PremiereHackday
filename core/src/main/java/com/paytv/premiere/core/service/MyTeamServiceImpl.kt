f000d8f602856dea05a0164dea3e993966ccd297
victorino <giovanna.victorino@gmail.com>
2021-05-14 12:25:40 -0300

    Adding MyTeam.json and MyTeamServiceImpl

---
+++ core/src/main/java/com/paytv/premiere/core/service/MyTeamServiceImpl.kt
@@ -0,0 +1,18 @@
+package com.paytv.premiere.core.service
+
+import android.content.Context
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.paytv.premiere.core.entities.Match
+
+class MyTeamServiceImpl(private val context: Context) : MatchesService {
+    override fun getMatches(): List<Match> {
+        val json = context.resources.assets.open(JSON_PATH).reader()
+        val itemType = object : TypeToken<List<Match>>() {}.type
+        return Gson().fromJson(json, itemType)
+    }
+
+    companion object {
+        const val JSON_PATH = "myteam.json"
+    }
+}
